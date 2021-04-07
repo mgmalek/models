10b3fad7fe86a2cc9c337b5a4cebd81e30e6cb49
Michael Malek <michaelgmalek@gmail.com>
2021-04-07 11:05:04 +1000

    Minor fixes

--- mixture-density-networks/src/mdn.py
+++ mixture-density-networks/src/mdn.py
@@ -28,39 +28,38 @@ class MixtureDensityNetwork(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         features = self.feature_extractor(x)
         means = self.mean_network(features).reshape(-1, self.num_gaussians, self.output_dim)
-        logvars = self.logvar_network(features).reshape(-1, self.num_gaussians)
+        logvars = self.logvar_network(features).reshape(-1, self.num_gaussians).unsqueeze(-1)
         weights = self.weight_network(features).reshape(-1, self.num_gaussians)
-        variances = logvars.exp()
+        stds = (logvars / 2).exp()
 
-        normals = Normal(means, variances.unsqueeze(-1))
+        normals = Normal(means, stds)
         weight_dist = Categorical(probs=weights)
 
         return normals, weight_dist
     
     def loss_func(self, targs: torch.Tensor, normals: Normal, weight_dist: Categorical):
         """Return the negative log-likelihood loss"""
-        reshaped_targs = targs.unsqueeze(1).expand_as(normals.loc)
+        assert len(targs.shape) == 2, "The shape of targs must be (batch, output_dim)"
+        reshaped_targs = targs.unsqueeze(1).expand_as(normals.loc)  # Shape: (batch, num_gaussians, output_dim)
 
         # Since each output dim of the multivariate Gaussian is independent, we
         # find the log probability of the target value by summing the log
-        # probabilities along each dimension. This is equivalent to multiplying
-        # the likelihoods and then taking their logarithm)
-        log_probs = normals.log_prob(reshaped_targs)
-        log_probs = log_probs.sum(-1)
-        log_probs += weight_dist.probs.log() # Equivalent to multiplying by weights then taking the log
+        # probabilities along each dimension.
+        log_probs = normals.log_prob(reshaped_targs).sum(-1)  # Shape: (batch, num_gaussians)
+        log_probs += weight_dist.probs.log()  # Equivalent to multiplying by weights then taking the log
         log_likelihoods = torch.logsumexp(log_probs, dim=1)
         nll = -log_likelihoods.mean()
         
         return nll
     
     def sample(self, x: torch.Tensor) -> torch.Tensor:
-        """Perform a forward pass and then sample from the outputs distributions"""
+        """Perform a forward pass and then sample from the output distribution"""
         with torch.no_grad():
             normals, weight_dist = self(x)
 
-        idxs = weight_dist.sample()
-        idxs = idxs.unsqueeze(-1)
-        idxs = torch.stack([idxs, idxs], dim=-1)
-        preds = normals.loc.gather(1, idxs)
-
-        return preds
+        idxs = weight_dist.sample()  # Determine which Gaussian to use for this sample
+        idxs = idxs[:, None, None]
+        idxs = idxs.repeat(1, 1, 2)  # Shape: (batch, 1, output_dim)
+        sample = normals.sample().gather(1, idxs)  # Sample from the relevant Gaussian
+        sample = sample.squeeze(1)   # Shape: (batch, output_dim)
+        return sample
